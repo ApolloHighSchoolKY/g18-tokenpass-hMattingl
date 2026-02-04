c4dc222a606861ce111870ae1413ecaebbf9b52a
hMattingl <hayden.mattingly2@stu.daviess.kyschools.us>
2026-02-04 17:53:42 +0000

    commit

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,28 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
-  
+  board = new int[playerCount];
+
+  for(int i = 0; i < board.length;i++)
+    board[i] = int((math.random()+ 1) * 10);
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
+  if(currentPlayer == board.length - 1)
+    int givenPlayer = 0;
+  else
+    int givenPlayer = currentPlayer + 1;
+
+  while(board[currentPlayer] > 0)
+  {
+    board[givenPlayer] += 1;
+    board[currentPlayer] -= 1;
+    
+    if(givenPlayer == board.length - 1)
+      givenPlayer = 0;
+    else
+      givenPlayer += 1;
+
+  }
   
 }
